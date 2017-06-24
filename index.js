121c08a57246a8aef7b1e478d76837b3d11f205e
Adrian Biedrzycki <a@modules.io>
2017-06-24 01:39:27 -0700

    Use svgize for image data to svg conversion

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const CacheLru = require('cache-lru');
 const Alea = require('alea');
+const svgize = require('svgize');
 
 const cache = new CacheLru({
   max: 128,
@@ -243,57 +244,6 @@ function makeCreature(seed, format, opts) {
         }
       }
 
-      function imageDataToSvg(imageData) {
-        const width = (opts && opts.width) ? opts.width : '';
-        const height = (opts && opts.height) ? opts.height : '';
-        const viewBox = (opts && opts.viewBox) ? opts.viewBox : '';
-        const style = (opts && opts.style) ? opts.style : '';
-
-        let result = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" ${width ? `width="${width}"` : ''} ${height ? `height="${height}"` : ''} ${viewBox ? `viewBox="${viewBox}"` : ''} ${style ? `style="${style}"` : ''} shape-rendering="crispEdges">`;
-
-        const colorHistories = {};
-        const {data: imageDataData} = imageData;
-        for (let y = 0; y < imageData.height; y++) {
-          for (let x = 0; x < imageData.width; x++) {
-            const baseIndex = (x + (y * imageData.width)) * 4;
-            const r = imageDataData[baseIndex + 0];
-            const g = imageDataData[baseIndex + 1];
-            const b = imageDataData[baseIndex + 2];
-            const a = imageDataData[baseIndex + 3];
-            const colorString = `rgba(${r},${g},${b},${a})`;
-
-            let entry = colorHistories[colorString];
-            if (!entry) {
-              entry = [];
-              colorHistories[colorString] = entry;
-            }
-
-            const colorPoint = new ColorPoint(x, y);
-            entry.push(colorPoint);
-          }
-        }
-        for (const colorString in colorHistories) {
-          const colorPoints = colorHistories[colorString];
-
-          result += `<path fill="${colorString}" d="`;
-
-          for (let i = 0; i < colorPoints.length; i++) {
-            const colorPoint = colorPoints[i];
-            const {x, y} = colorPoint;
-            if (i > 0) {
-              result += ' ';
-            }
-            result += `M${x},${y} h1 v1 h-1 z`;
-          }
-
-          result += `"></path>`;
-        }
-
-        result += '</svg>';
-
-        return result;
-      }
-
       const canvas = document.createElement('canvas');
       canvas.width = SIZE;
       canvas.height = SIZE;
@@ -320,7 +270,7 @@ function makeCreature(seed, format, opts) {
         ctx.putImageData(imageData, 0, 0);
         return canvas;
       } else if (format === 'svg') {
-        return imageDataToSvg(imageData);
+        return svgize.imageDataToSvg(imageData);
       } else {
         return null;
       }
